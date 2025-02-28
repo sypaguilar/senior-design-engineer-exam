9a961826c8870f77fb19653565c76f05a4f60b8f
Sophia Ysabelle Aguilar <sypaguilar@gmail.com>
2025-02-28 23:52:06 +0800

    022825-03: Simplify Cards;

--- src/Cards.tsx
+++ src/Cards.tsx
@@ -1,24 +1,24 @@
 import React from "react";
 import "./App.css";
 
-function Cards({data}) {
+function Cards({ data }) {
     return (
-        <div className="flex flex-col justify-center items-center p-8">
-            <div className={`flex`}>
-                <img
-                    src={data.imageUrl}
-                    alt="Social Post"
-                />
-            </div>
-            <div className="items-center">
-                <p className={`flex font-grotesqueBold text-5xl`}>{data.title}</p>
-                <div className="flex justify-center items-center">
-                    <p className={`flex font-grotesqueRegular text-xl items-center`}>For</p>
-                    <p className={`flex font-grotesqueBold text-2xl items-center`}>${data.price}</p>
+        <div className="p-10 w-full">
+            <div className="flex flex-col justify-center items-center p-10">
+                <div className={`flex`}>
+                    <img
+                        src={data.imageUrl}
+                        alt="Social Post"
+                    />
                 </div>
-            </div>
-            <div className="flex w-full justify-center items-center">
-            <button className={`bg-limeGreen w-full font-grotesqueRegular`}>Get Started</button>
+                <div className="flex flex-col items-center">
+                    <p className={`font-grotesqueBold text-5xl`}>{data.title}</p>
+                    <div className="flex flex-row justify-center items-center">
+                        <p className={`font-grotesqueRegular text-sm p-1`}>For</p>
+                        <p className={`font-grotesqueBold text-2xl p-1`}>${data.price}</p>
+                    </div>
+                    <button className={`bg-limeGreen w-full font-grotesqueRegular`}>Get Started</button>
+                </div>
             </div>
         </div>
     );
